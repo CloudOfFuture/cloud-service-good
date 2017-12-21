775c618aa8e5873973331fa44e4ccbf19d612014
MoonHong <1049313183@qq.com>
2017-12-21 19:02:18 +0800

    创建商品

---
+++ src/main/java/com/kunlun/good/service/GoodService.java
@@ -0,0 +1,53 @@
+package com.kunlun.good.service;
+
+import com.kunlun.entity.Good;
+import com.kunlun.result.DataRet;
+import com.kunlun.result.PageResult;
+
+import java.util.Date;
+
+/**
+ * @author by hmy
+ * @version <0.1>
+ * @created on 2017-12-21.
+ */
+public interface GoodService {
+
+    /**
+     * 创建商品
+     *
+     * @param good
+     * @return
+     */
+    DataRet<String> add(Good good);
+
+    /**
+     * 获取详情
+     *
+     * @param id
+     * @return
+     */
+    DataRet<Good> findById(Long id);
+
+
+    /**
+     * 分页查询
+     *
+     * @param pageNo
+     * @param pageSize
+     * @param searchKey
+     * @param goodNo
+     * @param startDate
+     * @param endDate
+     * @param brandId
+     * @param onSale
+     * @param categoryId
+     * @param hot
+     * @param isNew
+     * @param freight
+     * @return
+     */
+    PageResult findByCondition(Integer pageNo, Integer pageSize, String searchKey, String goodNo,
+                               Date startDate, Date endDate, Long brandId, String onSale, Long categoryId,
+                               String hot, String isNew, String freight);
+}
